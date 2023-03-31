cd1dfd5283feb3df725ddda44933a86c05cf5de2
Alamin <alamin017514@gmail.com>
2023-03-31 04:55:30 -0500

    added header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <h3>Programming Knowledge</h3>
+            <div>
+                <img src="https://randomuser.me/api/portraits/med/women/26.jpg" alt="" />
+            </div>
+        </div>
+
+    );
+};
+
+export default Header;
